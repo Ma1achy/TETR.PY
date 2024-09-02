e9270a80ea59c08f768f70114f4ec106ba5dc779
Malachy <MalachyDoherty@me.com>
2024-09-02 03:54:02 +0100

    updated comments

--- render.py
+++ render.py
@@ -1,4 +1,5 @@
 from matrix import Matrix
+from four import Four
 import pygame
 from pygame_config import PyGameConfig
 from utils import lerpBlendRGBA
@@ -26,13 +27,16 @@ class Render():
         """
         return (self.config.WINDOW_WIDTH - self.config.FOUR_INSTANCE_WIDTH) // 2, (self.config.WINDOW_HEIGHT - self.config.FOUR_INSTANCE_HEIGHT) // 2
 
-    def render_frame(self, MATRIX:Matrix):
+    def render_frame(self, four:Four):
         """
-        Render the frame
+        Render the frame of the Four Instance
+        
+        args:
+        four (Four): the Four instance to render
         """
         self.window.fill((0, 0, 0))
         self.window.blit(self.four_surface, (self.__get_four_coords_for_window_center()))
-        self.__render_matrix(MATRIX)
+        self.__render_matrix(four.matrix)
         self.__update_window()
     
     def __update_window(self):
@@ -44,6 +48,9 @@ class Render():
     def __draw_grid(self, matrix_surface_rect:pygame.Rect):
         """
         Draw the grid in the background of the matrix
+        
+        args:
+        matrix_surface_rect (pygame.Rect): the rectangle that the matrix is drawn in
         """
         grid_colour = lerpBlendRGBA((0, 0, 0), (255, 255, 255), 0.25)
         
@@ -62,6 +69,12 @@ class Render():
     def __draw_blocks(self, matrix:Matrix, matrix_surface_rect:pygame.Rect, transparent:bool, alpha:float):
         """
         Draw the blocks in the matrix
+        
+        args:
+        matrix (Matrix): the matrix to draw the blocks from
+        matrix_surface_rect (pygame.Rect): the rectangle that the matrix is drawn in
+        transparent (bool): whether the blocks should be transparent
+        alpha (float): the alpha value of the transparency
         """
         for i, row in enumerate(matrix):
             for j, value in enumerate(row):
@@ -76,6 +89,9 @@ class Render():
     def __draw_border(self, matrix_rect:pygame.Rect):
         """
         Draw the border around the matrix
+        
+        args:
+        matrix_rect (pygame.Rect): the rectangle that the matrix is drawn in
         """
         pygame.draw.line(self.four_surface, (255, 255, 255), 
                  (self.config.MATRIX_SCREEN_CENTER_X - self.config.BORDER_WIDTH // 2 - 1, self.config.MATRIX_SCREEN_CENTER_Y), 
@@ -94,7 +110,7 @@ class Render():
                         (self.config.MATRIX_SCREEN_CENTER_X + matrix_rect.width + self.config.BORDER_WIDTH - 1, self.config.MATRIX_SCREEN_CENTER_Y + matrix_rect.height + self.config.BORDER_WIDTH // 2 - 1), 
                         self.config.BORDER_WIDTH)
         
-    def __render_matrix(self, MATRIX:Matrix):
+    def __render_matrix(self, matrix:Matrix):
         """
         Render the matrix onto the window
         
@@ -103,9 +119,9 @@ class Render():
         """
         matrix_surface_rect = pygame.Rect(self.config.MATRIX_SCREEN_CENTER_X, self.config.MATRIX_SCREEN_CENTER_Y, self.config.MATRIX_SURFACE_WIDTH, self.config.MATRIX_SURFACE_HEIGHT)
              
-        self.__draw_blocks(MATRIX.ghost_blocks, matrix_surface_rect, transparent = True, alpha = 0.33)
+        self.__draw_blocks(matrix.ghost_blocks, matrix_surface_rect, transparent = True, alpha = 0.33)
         self.__draw_grid(matrix_surface_rect)
-        self.__draw_blocks(MATRIX.matrix, matrix_surface_rect, transparent = False, alpha = 1)
-        self.__draw_blocks(MATRIX.piece, matrix_surface_rect, transparent = False, alpha = 1)
+        self.__draw_blocks(matrix.matrix, matrix_surface_rect, transparent = False, alpha = 1)
+        self.__draw_blocks(matrix.piece, matrix_surface_rect, transparent = False, alpha = 1)
         
         self.__draw_border(pygame.Rect(self.config.MATRIX_SCREEN_CENTER_X, self.config.MATRIX_SCREEN_CENTER_Y, self.config.MATRIX_SURFACE_WIDTH, self.config.MATRIX_SURFACE_HEIGHT)) 
