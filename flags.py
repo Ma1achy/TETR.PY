3874d957cec8db2d363256a1c131183f26853ce5
Malachy <MalachyDoherty@me.com>
2024-09-02 03:16:56 +0100

    refactoring

--- flags.py
+++
@@ -1,2 +0,0 @@
-T_SPIN_FLAG = None # Can either be 'T-Spin' or 'T-Spin Mini'
-ON_FLOOR = False # if the piece is on the floor
--- sound.py
+++
@@ -1,6 +0,0 @@
-import pygame
-
-def play_t_spin_sound():
-    pygame.mixer.music.load("SE/t_spin.wav")
-    pygame.mixer.music.set_volume(0.20)
-    pygame.mixer.music.play()
