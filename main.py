145f945420249f9a4483eeb23817c9b793703934
Malachy <MalachyDoherty@me.com>
2024-09-14 04:42:53 +0100

    .

--- main.py
+++ main.py
@@ -3,14 +3,24 @@ from pygame_instance import PyGameInstance
 from four import Four
 
 #TODO:
+# === Handling ===
 # - Implement key priority for left/right movement, i.e if right is held and ten left is pressed, the left action should be prioritized (most recent action)
-# - GAME OVER CONDITIONS
-# - GRAVITY
-# - LOCK DELAY
+
+# - Implement GRAVITY
+# - Soft Drop Facor: The factor the soft dropping scales the current gravity by
+
+# - Implement Prefer Soft Drop Over Movement: At very high speeds, the soft drop action will be prioritized over movement
+
+# - LOCK DELAY MECHANICS & LOCKING (yes game logic but need this for SDF and PrefSD)
+
+# - Implement Prevent Accidental Hard Drops: When a piece locks on its own, the harddrop action is disabled for a few frames
+
+#  === game logic ===
 # - PERFECT CLEAR DETECTION
 # - COMBO DETECTION
 # - SCORING
 # - LEVELS
+# - GAME OVER CONDITIONS
 
 async def main():
     pygame_instance = PyGameInstance()
