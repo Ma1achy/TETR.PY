0957663f689f45bfd76be7bae13d34fdd777ac23
Malachy <MalachyDoherty@me.com>
2024-12-02 16:37:22 +0000

    .

--- app/mouse_input_handler.py
+++ app/mouse_input_handler.py
@@ -11,19 +11,19 @@ class MouseInputHandler():
         if not self.is_focused:
             return
         
-        self.queue_mouse_events({f'MB{event.button}':{'down': True, 'up': False, 'pos': event.pos}})
+        self.queue_mouse_events({f'mb{event.button}':{'down': True, 'up': False, 'pos': event.pos}})
       
     def on_mouse_up(self, event):
         if not self.is_focused:
             return
         
-        self.queue_mouse_events({f'MB{event.button}':{'down': False, 'up': True, 'pos': event.pos}})
+        self.queue_mouse_events({f'mb{event.button}':{'down': False, 'up': True, 'pos': event.pos}})
    
     def on_mouse_scroll(self, event):
         if not self.is_focused:
             return
         
-        self.queue_mouse_events({'scroll': {'x': event.precise_x, 'y': event.precise_y, 'inverted': event.flipped}}) # inverted is a bool that indicates if the scroll direction is inverted on the system
+        self.queue_mouse_events({'scrollwheel': {'x': event.precise_x, 'y': event.precise_y, 'inverted': event.flipped}}) # inverted is a bool that indicates if the scroll direction is inverted on the system
         
     def on_mouse_move(self, event):
         if not self.is_focused:
